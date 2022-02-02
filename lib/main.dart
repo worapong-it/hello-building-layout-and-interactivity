102dc2af9b5494894e7fcf0e9e98ae5a3fe63aa5
worapong-it <74828411+worapong-it@users.noreply.github.com>
2022-02-02 19:42:30 +0700

    feat: add button section (icon: call, route, share)

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,7 @@ class MyApp extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
-                  // padding: const EdgeInsets.only(bottom: 8),
-                  padding: const EdgeInsets.only(bottom: 0),
+                  padding: const EdgeInsets.only(bottom: 8),
                   child: const Text(
                     'Oeschinen Lake Campground',
                     style: TextStyle(fontWeight: FontWeight.bold),
@@ -49,6 +48,18 @@ class MyApp extends StatelessWidget {
       ),
     );
 
+    // --- button section --- //
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
     // --- return in build() --- //
     return MaterialApp(
       title: 'Flutter layout demo',
@@ -59,9 +70,32 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        const SizedBox(height: 10),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
