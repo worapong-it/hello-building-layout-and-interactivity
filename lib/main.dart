65c846c04f887be9066014de7ed94f1d1403b03b
worapong-it <74828411+worapong-it@users.noreply.github.com>
2022-02-02 20:14:17 +0700

    feat: add text section (long description, step 04)

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-// --- title section --- //
+    // --- title section --- //
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32),
       child: Row(
@@ -60,6 +60,23 @@ class MyApp extends StatelessWidget {
       ],
     );
 
+    // --- text section --- //
+    const String description =
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.';
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        description,
+        softWrap: true,
+      ),
+    );
+
     // --- return in build() --- //
     return MaterialApp(
       title: 'Flutter layout demo',
@@ -71,6 +88,7 @@ class MyApp extends StatelessWidget {
           children: [
             titleSection,
             buttonSection,
+            textSection,
           ],
         ),
       ),
